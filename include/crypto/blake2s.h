42c6098e50e70bdf229a61f7c77595bb707703e3
Bruce Teng <torai.d.l@outlook.com>
2024-04-10 14:57:40 +0800

    Revert "lib/crypto: blake2s: move hmac construction into wireguard"
    This reverts commit 80cd50288a02ac84224b534669578426c8b2deb2.

--- include/crypto/blake2s.h
+++ include/crypto/blake2s.h
@@ -99,4 +99,7 @@ static inline void blake2s(u8 *out, const u8 *in, const u8 *key,
 	blake2s_final(&state, out);
 }
 
-#endif /* _CRYPTO_BLAKE2S_H */
+void blake2s256_hmac(u8 *out, const u8 *in, const u8 *key, const size_t inlen,
+		     const size_t keylen);
+
+#endif /* BLAKE2S_H */
